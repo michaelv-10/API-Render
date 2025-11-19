f99f4107a165af442dfd3e606570d427e0488d83
michaelv-10 <michael.v211011@gmail.com>
2025-11-19 11:58:28 +1000

    Port added in start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #start.sh - start the FastAPI app on the port provided by the host
 
-uvicorn main:app --host 0.0.0.0 --port $PORT
+uvicorn main:app --host 0.0.0.0 --port ${PORT:-8000} --workers 1
